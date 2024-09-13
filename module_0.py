61d3ae4a4b9eab9d8038f29f6c11a14e31516fa3
SantiRod247 <santiagorod247@gmail.com>
2024-09-13 16:28:49 -0300

    Add initial code for working with torch tensors

---
+++ module_0.py
@@ -0,0 +1,13 @@
+import torch 
+# Scalar
+scalar = torch.tensor(7)
+print(scalar)
+print(scalar.ndim)
+print(scalar.item())
+# Vector
+vector = torch.tensor([7, 7])
+print(vector)
+# Check the number of dimensions of vector
+print(vector.ndim)
+print(# Check shape of vector
+vector.shape)
---
+++ torch_test.py
@@ -0,0 +1,4 @@
+import torch # type: ignore
+x = torch.rand(5, 3)
+print(x)
+torch.__version__
